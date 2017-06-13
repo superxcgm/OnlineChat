79d9ac8dacab1d73ddf84b0c6af59f8d95a93ec2
superxc <superxc@outlook.com>
2017-06-13 20:56:31 +0800

    update home.html

--- static/js/home.js
+++ static/js/home.js
@@ -1,8 +1,12 @@
 // init
+var screenHeight;
+var screenWidth;
 $(function(){
-	$("#left").height(document.body.scrollHeight);	
-	$("#right").height(document.body.scrollHeight);	
-	$("#right").outerWidth(document.body.scrollWidth - $("#left").outerWidth());
+	screenHeight = document.body.scrollHeight;
+	screenWidth = document.body.scrollWidth;
+	$("#left").height(screenHeight);	
+	$("#right").height(screenHeight);	
+	$("#right").outerWidth(screenWidth - $("#left").outerWidth());
 	$("#cUserInfo>.option").click(function(){
 		if($("#cUserInfo>.option>.option-panel").css("display") == "none")
 			$("#cUserInfo>.option>.option-panel").css("display", "block");
@@ -22,4 +26,63 @@ $(function(){
 			$("#room-list").css("display", "block");
 		}
 	});
+	// $("#right>.msg-content").height(screenHeight - $("#right>.msg-sent").height() - $("#right>.msg-username").height());
 });
+function sendMsg(obj)
+{
+	var userType = $("#right>.userType").val();
+	var userId = $("#right>.userId").val();
+	var content = $("#right>.msg-sent>textarea").val();
+	var msgItem = '\
+				<div class="msg-item">\
+					<div class="float-right">\
+						<img src="../static/img/soccor-80-80.jpg" alt="" class="portrait">\
+					</div>\
+					<div class="msg-detail float-right round">' + content + '</div>\
+				</div>';
+				// <div class="msg-item">
+				// 	<div class="float-left">
+				// 		<img src="../static/img/soccor-80-80.jpg" alt="" class="portrait">
+				// 	</div>
+				// 	<div></div>
+				// 	<div class="msg-detail float-left round">不在</div>
+				// </div>
+	var beforeHtml = $("#right>.msg-content").html();
+	$("#right>.msg-content").html(beforeHtml + msgItem);
+	$("#right>.msg-sent>textarea").val("");
+}
+function chat(obj)
+{
+	var userType = $(obj).children(".userType").val();
+	var userId = $(obj).children(".userId").val(); 
+	var userName = $(obj).find(".nickname").text().trim();
+	// console.log(userType);
+	// console.log(userId);
+	// console.log(userName);
+	var rightArea = '\
+			<input type="hidden" class="userType" value="" />\
+			<input type="hidden" class="userId" value="" />\
+			<div class="msg-username center">\
+				启宏与蛙\
+			</div>\
+			<div class="msg-content">\
+				\
+			</div>\
+			<div class="msg-sent">\
+				<textarea></textarea>\
+				<button class="btn btn-primary" style="width: 80px;" onclick="sendMsg(this)">发送</button>\
+			</div>';
+	$("#right").html(rightArea);
+	$("#right>.msg-content").height(screenHeight - $("#right>.msg-sent").height() - $("#right>.msg-username").height());
+	$("#right>.msg-username").text(userName);
+	$("#right>.userType").val(userName);
+	$("#right>.userId").val(userId);
+
+	$("#right>.msg-sent>textarea").keydown(function(event){
+		/* Enter */
+		console.log(event.keyCode);
+		if(event.keyCode == 13){
+			sendMsg();
+		}
+	});
+};
